915c18f9d249c64c9c3e7bdf3cba9d35c29852e4
Jacob Arrington <Jacobsarrington@gmail.com>
2023-01-30 00:41:49 -0500

    signup fix

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const { setTokenCookie, requireAuth } = require('../../utils/auth');
-const { User } = require('../../db/models');
+const { User, sequelize } = require('../../db/models');
 const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
+const {Op} = require('sequelize')
 const router = express.Router();
 
 
@@ -27,41 +28,31 @@ const validateSignup = [
   ];
 
 // Sign up
+
 router.post(
-    '/',
-    validateSignup,
-    async (req, res) => {
-      const { firstName, lastName, email, password, username } = req.body;
-      const user = await User.signup({  firstName, lastName, email, username, password });
-      const validEmail = await User.findOne({where:[{email},]})
-     
-    if(validEmail){
-      res.status(403)
-      return res.json({
-        message: "User already exists",
-        statusCode: 403,
-        errors: {
-          "email": "User with that email already exists"
-        }
-      })
-    }
-    const validUser = await User.findOne({where:[{username},]})
+  '/',
+  validateSignup,
+  async (req, res) => {
+
+    const { email, password, username, firstName, lastName } = req.body;
+    const validUser = await User.findOne({where:{[Op.or]: [{email}, {username}]}})
+
     if(validUser){
       res.status(403)
-      return res.json({
-        message: "User already exists",
-        statusCode: 403,
-        errors: {
-          "email": "User with that email already exists"
-        }
-      })
+      return res.json({message:"User already exists", statusCode: 403, errors:[
+        "User with email and/or username already exists"
+      ]})
     }
-        // vscode lies 
-      await setTokenCookie(res, user);
+    
+    const user = await User.signup({ email, username, password, firstName, lastName });
 
-      return res.json({
-        user
-      });
-    }
-  );
+    await setTokenCookie(res, user);
+
+    return res.json({
+      user: user
+    });
+  }
+);
+
+
 module.exports = router;
